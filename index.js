8a96155f812f2c6f5da5608f2c01d8b12c319383
booo <borgers@mi.fu-berlin.de>
2011-07-25 21:53:12 +0200

    export what we need in the index file

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.Geometry = require('./build/default/binding').Geometry
+exports.Projection = require('./build/default/binding').Projection
+exports.StrTree = require('./build/default/binding').StrTree
