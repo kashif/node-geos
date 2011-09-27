8a011036b478f94274971c2bb689d5bb8341a77d
booo <borgers@mi.fu-berlin.de>
2011-09-27 14:56:45 +0200

    geos.js: do not export strtree and projection

--- geos.js
+++ geos.js
@@ -1,3 +1,1 @@
-exports.Geometry = require('./build/default/geos').Geometry
-exports.Projection = require('./build/default/geos').Projection
-exports.StrTree = require('./build/default/geos').StrTree
+exports.Geometry = require('./build/default/geos').Geometry;
