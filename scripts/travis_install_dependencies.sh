733d2fe05a6c8f298cfbe87b4774c0c83116afea
booo <borgers@mi.fu-berlin.de>
2012-03-15 00:38:13 +0100

    travis: reduce verbosity of build script

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -3,14 +3,16 @@
 if [ "$LIB_GEOS_VERSION" = "git" ]
 then
     git clone https://github.com/strk/geos.git \
-    && cd geos
+    && cd geos && ./autogen.sh > /dev/null
 elif [ "$LIB_GEOS_VERSION" = "3.3.2" ]
 then
     wget http://download.osgeo.org/geos/geos-3.3.2.tar.bz2 \
-    && tar -xvf geos-3.3.2.tar.bz2 \
+    && tar -xf geos-3.3.2.tar.bz2 \
     && cd geos-3.3.2
 else
     exit 1
 fi
 
-./configure --prefix=/user/ && make -j4 && sudo make install && cd ..
+./configure --prefix=/user/ > /dev/null && make -j4 > /dev/null \
+    && sudo make install > /dev/null \
+    && cd ..
