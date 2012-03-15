842646f2762e04a785b4c0d76e579cea9cb68b32
booo <borgers@mi.fu-berlin.de>
2012-03-15 02:23:28 +0100

    travis: reduce verbosity of aptitude update

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -3,4 +3,4 @@
 wget http://apt.opengeo.org/gpg.key \
     && sudo apt-key add gpg.key \
     && sudo sh -c "echo \"deb http://apt.opengeo.org/ubuntu lucid main\" >> /etc/apt/sources.list" \
-    && sudo aptitude update && sudo aptitude -y -P install libgeos-dev
+    && sudo aptitude update > /dev/null && sudo aptitude -y -P install libgeos-dev
