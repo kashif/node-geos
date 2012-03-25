5c94a73a6780c6bfa5fdb68126a150fc3f59d6b9
Andreas Kalsch <andreaskalsch@gmx.de>
2012-03-25 14:49:20 +0200

    Refactoring of the GeoJSONWriter class: - header order - setBBox -> setBbox - added a writeBbox method, including binding - ::New is public - added setBbox method to make it consistent with JS

--- src/cpp/geojsonwriter.hpp
+++ src/cpp/geojsonwriter.hpp
@@ -10,40 +10,43 @@
 #include <geos/geom/MultiLineString.h>
 #include <geos/geom/MultiPolygon.h>
 #include "binding.hpp"
+#include "geometry.hpp"
 
 class GeoJSONWriter : public ObjectWrap{
 
-    public:
-        bool bbox;
+    /*public:*/
 
     protected:
         int decimalPlaces;
         double factor;
+        bool bbox;
+
+    private:
+        static Persistent<FunctionTemplate> constructor;
 
     public:
         GeoJSONWriter();
         ~GeoJSONWriter();
-        void setRoundingPrecision (int places);
+        void setRoundingPrecision(int places);
+        void setBbox(bool bbox);
         Handle<Value> write(const geos::geom::Geometry* geom);
-        static void Initialize(Handle<Object> target);
+        Handle<Value> writeBbox(const geos::geom::Geometry* geom);
 
-    protected:
+        static void Initialize(Handle<Object> target);
         static Handle<Value> New(const Arguments& args);
 
+    protected:
         double roundNumber(double coord);
         Handle<Array> coordinateToArray(const geos::geom::Coordinate* coord);
         Handle<Array> coordinateSequenceToArray(const geos::geom::CoordinateSequence* seq);
         Handle<Array> geometryCollectionToArrayOfArrays(const geos::geom::GeometryCollection* geom);
         Handle<Array> geometryCollectionToArrayOfObjects(const geos::geom::GeometryCollection* geom);
         Handle<Value> getCoordsOrGeom(const geos::geom::Geometry* geom);
-        Handle<Value> getBbox(const geos::geom::Geometry* geom);
 
-        static Handle<Value> Write(const Arguments& args);
         static Handle<Value> SetRoundingPrecision(const Arguments& args);
-        static Handle<Value> SetBBox(const Arguments& args);
-
-    private:
-        static Persistent<FunctionTemplate> constructor;
+        static Handle<Value> SetBbox(const Arguments& args);
+        static Handle<Value> Write(const Arguments& args);
+        static Handle<Value> WriteBbox(const Arguments& args);
 
 };
 
