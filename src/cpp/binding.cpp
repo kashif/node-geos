3b650723d9365c69eebbdafe07d18f590c7d0e01
booo <borgers@mi.fu-berlin.de>
2012-04-16 18:07:17 +0200

    cpp/binding: use NODE_MODULE

--- src/cpp/binding.cpp
+++ src/cpp/binding.cpp
@@ -8,19 +8,22 @@
 #include "geojsonwriter.hpp"
 #include "geojsonreader.hpp"
 
-extern "C"
-void init (Handle<Object> target) {
-    HandleScope scope;
+extern "C" {
+    void init (Handle<Object> target) {
+        HandleScope scope;
 
-    target->Set(String::NewSymbol("geosversion"), String::NewSymbol(geos::geom::geosversion().data()));
-    target->Set(String::NewSymbol("jtsport"), String::NewSymbol(geos::geom::jtsport().data()));
+        target->Set(String::NewSymbol("geosversion"), String::NewSymbol(geos::geom::geosversion().data()));
+        target->Set(String::NewSymbol("jtsport"), String::NewSymbol(geos::geom::jtsport().data()));
 
-    Geometry::Initialize(target);
-    WKTReader::Initialize(target);
-    WKTWriter::Initialize(target);
-    GeometryFactory::Initialize(target);
-    PrecisionModel::Initialize(target);
-    Quadtree::Initialize(target);
-    GeoJSONWriter::Initialize(target);
-    GeoJSONReader::Initialize(target);
+        Geometry::Initialize(target);
+        WKTReader::Initialize(target);
+        WKTWriter::Initialize(target);
+        GeometryFactory::Initialize(target);
+        PrecisionModel::Initialize(target);
+        Quadtree::Initialize(target);
+        GeoJSONWriter::Initialize(target);
+        GeoJSONReader::Initialize(target);
+    }
+
+    NODE_MODULE(geos, init)
 }
